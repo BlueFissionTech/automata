4054a2a25e7cd07e4f4545682a3be95baa614c33
Devon Scott <Devon Scott>
2016-11-07 10:11:48 -0500

    removed behavior pass by reference in organized behavior collection

--- src/Bot/Behaviors/OrganizedBehaviorCollection.php
+++ src/Bot/Behaviors/OrganizedBehaviorCollection.php
@@ -4,7 +4,7 @@ namespace BlueFission\Bot\Behaviors;
 use BlueFission\Bot\Collections\OrganizedCollection;
 
 class OrganizedBehaviorCollection extends OrganizedCollection {
-	public function add( &$behavior, $label = null ) {
+	public function add( $behavior, $label = null ) {
 		if (!$this->has($behavior->name()))
 			parent::add( $behavior, $behavior->name() );
 	}
