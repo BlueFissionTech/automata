1dc8820ab109f431a624fb7baa7cfe2548abb804
Devon Scott <dscott@bluefission.com>
2022-01-05 16:26:36 -0500

    added frame class

---
+++ src/Bot/Comprehension/Frame.php
@@ -0,0 +1,21 @@
+<?php
+namespace BlueFission\Bot\Comprehension;
+
+class Frame {
+	private $_experiences;
+
+	public function construct()
+	{
+		$this->_experiences = Collection();
+	}
+
+	public function addExperience( $experience, $source = null ) 
+	{
+		$this->_experiences[$source] = $experience;
+	}
+
+	public function extract() {
+		$values = $experience['values'];
+		return $values;
+	}
+}
