77aaebc60471047d3fa689ec51868189ebf804e6
Devon Scott <dscott@bluefission.com>
2023-06-22 17:52:33 -0400

    fixed type in Strategy.php

--- src/Bot/Strategies/Strategy.php
+++ src/Bot/Strategies/Strategy.php
@@ -6,7 +6,7 @@ abstract class Strategy implements IStrategy
     protected $testSamples;
     protected $testTargets;
 
-    abstract public function train((array $samples, array $labels, float $testSize = 0.2);
+    abstract public function train(array $samples, array $labels, float $testSize = 0.2);
     abstract public function predict($input);
     abstract public function accuracy(): float;
 }
